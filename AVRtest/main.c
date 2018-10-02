331280d0c12c0f4a326788a159f7272201845f5e
RAZMI <RAZMI@RAZMI-PC>
2018-10-02 12:32:44 +0530

    First commit

---
+++ AVRtest/main.c
@@ -0,0 +1,21 @@
+/*
+ * main.c
+ *
+ *  Created on: Oct 2, 2018
+ *      Author: RAZMI
+ */
+
+#include<avr/io.h>
+#include<util/delay.h>
+
+int main()
+{
+	DDRA = 0XF0;
+	PORTA = 0x00;
+	while(1)
+	{
+
+	}
+
+	return 0;
+}
